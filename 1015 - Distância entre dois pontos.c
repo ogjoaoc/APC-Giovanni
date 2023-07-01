dde28f44be801aace362f21b51adceacaf17cd82
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2023-07-01 17:02:10 -0300

    Create 1015 - Distância entre dois pontos.c

---
+++ 1015 - Distância entre dois pontos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() 
+
+{
+    
+  float x1,x2,y1,y2,resultado=0;
+
+  scanf("%f %f", &x1, &y1);
+  scanf("%f %f", &x2, &y2);
+
+  resultado = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+
+  printf("%.4f\n" , resultado);  
+    
+    
+    return 0;
+}
