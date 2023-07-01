cf2b5329848a764af36b028b53362082b2646a77
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2023-07-01 17:16:09 -0300

    Create 1037 - Intervalo.c

---
+++ 1037 - Intervalo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+double numero;
+scanf("%lf", &numero);
+    if((numero > 100 || numero < 0)) {
+        printf("Fora de intervalo\n");
+    }
+    else if((numero <= 25)) {   
+        printf("Intervalo [0,25]\n");
+
+    } else if((numero <= 50)) {
+        printf("Intervalo (25,50]\n");
+
+    }
+
+    else if((numero <= 75)) {
+        printf("Intervalo (50,75]\n");
+    }
+    else if((numero <= 100)) {
+        printf("Intervalo (75,100]\n");
+    }
+
+ 
+    return 0;
+}
